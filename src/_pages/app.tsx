16075d40086adf45a20fbd8519d9946b46d71d75
lucasDev88 <lucasfelipe300810@gmail.com>
2026-02-14 21:51:10 -0300

    Login and Signup page v1

--- src/_pages/app.tsx
+++ src/_pages/app.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./navigation/home"
 import "../style/App.css"
+import Login from "./navigation/login"
+import SignUp from "./navigation/SignUp"
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={ <Login /> } />
+                <Route path="/signup" element={ <SignUp /> } />
             </Routes>
         </BrowserRouter>
     )
